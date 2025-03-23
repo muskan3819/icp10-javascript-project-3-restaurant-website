1ac65456c23c6b46d6c875e7aa1b1d0856ccf98c
Bhosalevaishu <vb9714921@gmail.com>
2025-03-23 14:13:33 +0530

    changes in javascript

--- javascript/login.js
+++ javascript/login.js
@@ -1,14 +1,16 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
+document.getElementById('login-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var errorMessage = document.getElementById('errorMessage');
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
 
     // Sample validation: replace this with real validation or an API call
     if (username === "admin" && password === "123") {
-        window.location.href = "index.html";  // Redirect to another page on successful login
+        alert("Form submitted successfully");
+         window.location.href = "index.html";   // Redirect to another page on successful login
     } else {
-        errorMessage.textContent = "Invalid username or password.";
+        alert("please fill in both filds .");
+        errorMessage.textContent = " Invalid username and password.";
     }
 });
