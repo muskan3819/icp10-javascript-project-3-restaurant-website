f90dc2e67e4eba3f7591cfddcf54fd5493e23401
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-03-28 04:20:39 +0530

    popup variable is declared

--- javascript/reservation.js
+++ javascript/reservation.js
@@ -2,3 +2,4 @@ const seatsSelect = document.getElementById('seats');
 const tableTypeSelect = document.getElementById('table-type');
 const priceDisplay = document.getElementById('price-display');
 const reserveButton = document.getElementById('reserve-button');
+const popup = document.getElementById('popup');
