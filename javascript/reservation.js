e30b8edc77fb89f66efb13d9de70747ee631f065
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-03-28 04:22:15 +0530

    price js added

--- javascript/reservation.js
+++ javascript/reservation.js
@@ -4,3 +4,7 @@ const priceDisplay = document.getElementById('price-display');
 const reserveButton = document.getElementById('reserve-button');
 const popup = document.getElementById('popup');
 const popupButton = document.getElementById('popup-button');
+
+const pricePerSeat = 100;
+const interiorTablePrice = 50;
+const exteriorTablePrice = 0;
