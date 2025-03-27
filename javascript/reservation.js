3d77bb172cb2531288f4441d01ecfc23c2111349
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-03-28 04:20:13 +0530

    reserveButton variable is declared

--- javascript/reservation.js
+++ javascript/reservation.js
@@ -1,3 +1,4 @@
 const seatsSelect = document.getElementById('seats');
 const tableTypeSelect = document.getElementById('table-type');
 const priceDisplay = document.getElementById('price-display');
+const reserveButton = document.getElementById('reserve-button');
