f94caca124fda258be6667129f56a687d5a86a52
DnyaneshwariMagar <magardnyaneshwari242@gmail.com>
2025-03-27 19:39:13 +0530

    if-else added

--- javascript/maharashtrian-food.js
+++ javascript/maharashtrian-food.js
@@ -1,4 +1,9 @@
 let cart = [];
 function addToCart(itemName) {
     let existingItem = cart.find(item => item.name === itemName);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({ name: itemName, quantity: 1 });
+    }
 }
