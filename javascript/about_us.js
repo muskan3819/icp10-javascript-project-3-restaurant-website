bcd5ff15cd997cffe6a66bf8f315777523e3e78a
muskan3819 <muskanshaikh3819@gmail.com>
2025-03-28 16:23:06 +0530

    js code added

--- javascript/about_us.js
+++ javascript/about_us.js
@@ -64,3 +64,11 @@ resetBtn.addEventListener('click', () => {
   cardBack.style.display = 'none';
 });
 
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
